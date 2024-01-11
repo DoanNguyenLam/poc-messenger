1bc56c18f52eb16bf66b732b78c41d4bac4501d4
CaBien - [AIdaBOT] <lam-dn@genkisystem>
2024-01-11 17:14:02 +0700

    update fb chat

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
-import { ENV, MY_VERIFY_FB_TOKEN, PORT } from "./src/configs/application-props.js";
+import {
+  ENV,
+  MY_VERIFY_FB_TOKEN,
+  PORT,
+} from "./src/configs/application-props.js";
 
 const app = express();
 
@@ -33,6 +37,40 @@ app.get("/webhook/", function (req, res) {
       // Responds with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);
     }
+  }
+});
+
+app.post("/webhook", function (req, res) {
+  // Parse the request body from the POST
+  console.log('body', req.body);
+  let body = req.body;
+
+  // Check the webhook event is from a Page subscription
+  if (body.object === "page") {
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function (entry) {
+      // Gets the body of the webhook event
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log("Sender PSID: " + sender_psid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
+    });
+
+    // Return a '200 OK' response to all events
+    res.status(200).send("EVENT_RECEIVED");
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
   }
 });
 
